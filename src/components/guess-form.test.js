0f1b2621059d0855b0c6ded1adc68897ddbb011e
Taylor Hammons <taylor@thinkful.com>
2018-10-09 15:17:19 -0500

    added shallow test to guess-form

---
+++ src/components/guess-form.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+
+import GuessForm from './guess-form';
+
+describe('<GuessForm />', () => {
+    it('Renders without crashing' , () => {
+        shallow(<GuessForm />);
+    });
+});
+
